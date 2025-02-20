4ff56d4bb420ffff401f1619abbb4a32badc3ab5
RSathvika20 <7vikarodda@gmail.com>
2025-02-20 21:38:33 +0530

    Time: 3 ms (67.99%) | Memory: 7.8 MB (34.27%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,7 +5,18 @@ public:
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+       
+            int a=0;
+            int b=1;
+            
+            for(int i=2;i<=n;i++){
+                c=a+b;
+                a=b;
+                b=c;
+                
+            }
+        
+        return c;
         
     }
 };
