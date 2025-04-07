b408685772f3ecd966453e581d6e5d53cf304a60
RSathvika20 <7vikarodda@gmail.com>
2025-04-07 10:28:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (55.34%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int>indexs(200,0);
+        vector<int>indext(200,0);
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            if(indexs[s[i]]!=indext[t[i]]){
+                return false;
+            }
+            indexs[s[i]]=i+1;
+            indext[t[i]]=i+1;
+        }
+        return true;
+    }
+};
