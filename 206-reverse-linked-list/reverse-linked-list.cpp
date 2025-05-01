7dc3698dfbf8ac909e65653eb3729a22dd184d6e
RSathvika20 <7vikarodda@gmail.com>
2025-05-01 20:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (39.43%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -14,15 +14,10 @@ public:
        if(head==NULL || head->next==NULL){
         return head;
        }
-       ListNode* curr=head;
-       ListNode* prev=NULL;
-       while(curr!=NULL){
-        ListNode* temp=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=temp; 
-       }
-       return prev;
+       ListNode* rh=reverseList(head->next);
+       head->next->next=head;
+       head->next=NULL;
+       return rh;
         
     }
 };
