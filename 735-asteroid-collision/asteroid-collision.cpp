731996683bc19904134917ccc17d801a63981ba4
RSathvika20 <7vikarodda@gmail.com>
2025-08-15 20:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (89.98%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int>st;
+       for(int a:asteroids){
+        while(!st.empty() && st.back()>0 && a<0){
+             if (st.back() < -a) {
+                    st.pop_back();
+                    continue;
+                } else if (st.back() == -a) {
+                    st.pop_back();
+                }
+                a=0;
+                break;
+        }
+        if(a!=0){
+            st.push_back(a);
+        }
+       }
+        return st;
+        
+    }
+};
